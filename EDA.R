32755611a48e31810deaffa22b79d890f5ded606
potatchipsxp <meyertimothy909@gmail.com>
2020-12-08 12:10:37 -0800

    fix commit?

--- EDA.R
+++ EDA.R
@@ -20,6 +20,11 @@ colnames(dataR)
 
 vis_miss(dataS, warn_large_data = FALSE)
 
-smallDataS <- dataS %>%  
-  select(`Profit or Non-Profit`, `Chain Owned`, `Chain Organization`, `Linearized score of nephrologists' communication and caring`, `Star rating of quality of dialysis center care and operations`, `Star rating of quality of dialysis center care and operations`, yellowCards, ties) 
+Linear_smallDataS = dataS %>%  
+  select(`Provider Number`, `Profit or Non-Profit`, `Chain Owned`, `Chain Organization`, `Linearized score of nephrologists' communication and caring`, `Linearized score of quality of dialysis center care and operations`, `Linearized score of providing information to patients`, `Linearized score of rating of the nephrologist`, `Linearized score of rating of the dialysis center staff`, `Linearized score of rating of the dialysis facility`) %>% 
+  rename(PoNP = `Profit or Non-Profit`, Chain = `Chain Owned`,Org = `Chain Organization`, CnC = `Linearized score of nephrologists' communication and caring`,Operations = `Linearized score of quality of dialysis center care and operations`, info = `Linearized score of providing information to patients`, Neph =  `Linearized score of rating of the nephrologist`, Staff = `Linearized score of rating of the dialysis center staff`, Facility = `Linearized score of rating of the dialysis facility`)
 
+smallDataR = dataR %>% 
+  select(!c("Network","Facility Name","Five Star Date","Five Star","Five Star Data Availability Code","Address Line 1","Address Line 2","City","State","Zip","County","Phone Number",))
+
+ourData = left_join(Linear_smallDataS, smallDataR, "Provider Number")
